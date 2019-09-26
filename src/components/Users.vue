f80b2867084182d2eb34d411a010974bddfa4460
lin <test@test.com>
2019-09-26 16:50:02 +0800

    组件间通信：子组件向父组件传值, 通过 $emit (事件)

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -4,6 +4,8 @@
       <li v-for="user in users" :key="user">
         {{user}}
       </li>
+
+      <h1 @click="changeTitle">{{title}}</h1>
     </ul>
   </div>
 </template>
@@ -12,9 +14,21 @@
 export default {
   name: 'users',
   props:{
-    users:{           //这个就是父组件中子标签自定义名字
+    // 这个就是父组件中子标签自定义名字
+    users:{
       type:Array,
       required:true
+    }
+  },
+  data() {
+    return {
+      title:"Vue.js Demo",
+    }
+  },
+  methods:{
+    changeTitle() {
+      // 自定义事件  传递值“子向父组件传值”
+      this.$emit("titleChanged", "子向父组件传值");
     }
   }
 }
--- src/components/UsersParent.vue
+++ src/components/UsersParent.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <Users :users="users"></Users>
+    <Users :users="users" @titleChanged="updateTitle"></Users>
+    <h2>{{title}}</h2>
   </div>
 </template>
 
@@ -13,11 +14,17 @@ export default {
   name: 'UsersParent',
   data(){
     return{
-      users:["Henry", "Bucky", "Emily"]
+      users: ["Henry", "Bucky", "Emily"],
+      title: "传递的是一个值"
     }
   },
   components:{
     Users
-  }
+  },
+  methods:{
+    updateTitle(e){   //声明这个函数
+      this.title = e;
+    }
+  },
 }
 </script>
